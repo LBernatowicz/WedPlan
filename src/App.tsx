487a624f7d0ecae27f4a50aa06133e26629c4aca
LukaszBernatowicz <bernatowicz122@gmail.com>
2022-10-13 20:24:43 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView
+      style={{ justifyContent: 'center', alignItems: 'center', flex: 1 }}>
+      <Text>dsadasd</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
