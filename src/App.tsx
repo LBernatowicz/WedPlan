95b47e63e6e831f1b862625d9b7287b78004364c
LukaszBernatowicz <bernatowicz122@gmail.com>
2022-10-17 16:09:28 +0200

    Add ErrorBoundary.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,16 @@ import React from 'react';
 import Navigation from './navigation/Navigation';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { AnimatedScrollProvider } from './hooks/ui/useAnimatedScroll';
+import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary';
 
 const App = () => {
   return (
     <SafeAreaProvider>
-      <AnimatedScrollProvider>
-        <Navigation />
-      </AnimatedScrollProvider>
+      <ErrorBoundary>
+        <AnimatedScrollProvider>
+          <Navigation />
+        </AnimatedScrollProvider>
+      </ErrorBoundary>
     </SafeAreaProvider>
   );
 };
